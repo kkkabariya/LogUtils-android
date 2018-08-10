24faa242cab693a1a1328a23febd1359ac9aea1f
kkkabariya <kritesh.cmexpertise@gmail.com>
2018-08-10 17:47:18 +0530

    init message

---
+++ logutils/src/main/java/com/cmexpertise/android/logutils/LogDebug.java
@@ -0,0 +1,15 @@
+package com.cmexpertise.android.logutils;
+
+import android.util.Log;
+
+public class LogDebug {
+
+
+
+    public static void d(String name, String val){
+
+        Log.v(name,val);
+
+    }
+
+}
